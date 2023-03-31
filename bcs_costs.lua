a9946a29b0c8c93833fade6d5cd8f39a10f03685
Eisenmonoxid <29467637+Eisenmonoxid@users.noreply.github.com>
2023-03-31 20:26:32 +0200

    Refactored the costs file a bit

--- bcs_costs.lua
+++ bcs_costs.lua
@@ -1,67 +1,17 @@
+--[[
+	-> This function has to be called from the local script after the bcs_local.lua was loaded.
+	-> You can change all costs at every time in the game. 
+	-> The first good amount has to be equal or higher than the original costs.
+	-> The second good and amount can be chosen freely.
+		
+	-> Diese Funktion muss aus dem lokalen Skript aufgerufen werden nachdem bcs_local.lua geladen wurde.
+	-> Es können alle Gebäudekosten zu jedem Zeitpunkt im Spiel geändert werden.
+	-> Die Höhe der ersten Ware muss gleich oder höher dem Originalwert liegen!
+	-> Die zweite Ware und ihre Höhe können frei festgelegt werden.
+]]--
 function SetMyBuildingCosts()
-	--[[
-	
-		-> This function has to be called from the local script after the bcs_local.lua was loaded.
-		-> You can change all costs at every time in the game. 
-		-> The first good amount has to be equal or higher than the original costs.
-		-> The second good and amount can be chosen freely.
-		
-		-> Diese Funktion muss aus dem lokalen Skript aufgerufen werden nachdem bcs_local.lua geladen wurde.
-		-> Es können alle Gebäudekosten zu jedem Zeitpunkt im Spiel geändert werden.
-		-> Die Höhe der ersten Ware muss gleich oder höher dem Originalwert liegen!
-		-> Die zweite Ware und ihre Höhe können frei festgelegt werden.
-		
-		Example call from the local lua script:
-		
-		function Mission_LocalOnMapStart()
-			Script.Load("C:\\Path\\bcs_local.lua")
-			Script.Load("C:\\Path\\bcs_costs.lua")
+	LoadBuildingCostSystem() -- Do not comment this out, this loads the BCS. / Nicht auskommentieren, das hier lädt das BCS.
 
-			SetMyBuildingCosts() -- Set Custom Costs
-		end
-		
-	]]--
-	
-	-- Initialize BCS --
-	if BCS ~= nil then
-		-- Init System
-		BCS.InitializeBuildingCostSystem()
-		-- Add Savegame when QSB is present
-		if (API and QSB) then
-			-- Register Savegame
-			if API.AddSaveGameAction then -- QSB-S 2.x
-				GUI.SendScriptCommand([[
-					API.AddSaveGameAction(function()
-						Logic.ExecuteInLuaLocalState('BCS.InitializeBuildingCostSystem()')
-					end)
-				]])
-				Framework.WriteToLog("BCS: QSB-S 2.x found! AddSaveGameAction for Savegame registered!")
-			elseif API.AddScriptEventListener and QSB.ScriptEvents ~= nil then -- QSB-S 3.x
-				API.AddScriptEventListener(QSB.ScriptEvents.SaveGameLoaded, BCS.InitializeBuildingCostSystem)
-				API.AddScriptEventListener(QSB.ScriptEvents.BriefingEnded, BCS.OverwriteEndScreenCallback)
-				Framework.WriteToLog("BCS: QSB-S 3.x found! ScriptEventListener for Savegame and BriefingEnded registered!")
-			else
-				Framework.WriteToLog("BCS: QSB-S found, but no Savegame registered. Has to be done manually!")
-			end
-		else
-			Framework.WriteToLog("BCS: QSB-S NOT found! Savegamehandling has to be done manually!")
-		end
-		
-		-- If you don't use the QSB-S, you have to overwrite the Mission_OnSaveGameLoaded() in the global script.
-		-- And call Logic.ExecuteInLuaLocalState('BCS.InitializeBuildingCostSystem()') from there.
-		-- Otherwise all costs are gone after Saveload and the script will not work!
-		
-		-- Sollte die QSB-S nicht verwendet werden, muss Mission_OnSaveGameLoaded() im globalen Skript manuell überschrieben werden.
-		-- Dort muss dann Logic.ExecuteInLuaLocalState('BCS.InitializeBuildingCostSystem()') aufgerufen werden.
-		-- Ansonsten sind nach Saveload alle Kosten weg und das Skript wird nicht funktionieren!
-	else
-		local ErrorMessage = "ERROR: Could not load BuildingCostSystem!"
-		Framework.WriteToLog(ErrorMessage)
-		assert(false, ErrorMessage)
-		return;
-	end
-	-- Done --
-	
 	--< Set your own BuildingCosts here! / Setze deine eigenen Gebäudekosten hier! >--
 	--Gatherer - Farms
 	BCS.EditBuildingCosts(UpgradeCategories.CattleFarm, 15, Goods.G_Salt, 12)
@@ -112,32 +62,32 @@ function SetMyBuildingCosts()
 	--Palisade/Wall - Gates
 	BCS.EditBuildingCosts(UpgradeCategories.PalisadeGate, 10, Goods.G_Herb, 6)
 	BCS.EditBuildingCosts(GetUpgradeCategoryForClimatezone("WallGate"), 16, Goods.G_Grain, 6)
-	--Buildings without fixed cost
+	--Buildings without fixed cost (Wall, Palisade, Road, Trail)
 	BCS.EditWallCosts(3.2, Goods.G_Grain, 2.7) --Wallcosts/Mauerkosten (!NO REFUND)
 	BCS.EditPalisadeCosts(3.5, Goods.G_Carcass, 1.2) --Palisadecosts/Palisadenkosten (!NO REFUND)
 	BCS.EditRoadCosts(3, Goods.G_Leather, 1.8) --Roadcosts/Straßenkosten
 	BCS.EditTrailCosts(Goods.G_Herb, 1.5, Goods.G_Sausage, 1.1) --Streetcosts/Wegkosten
-	
+	--Festival
 	BCS.EditFestivalCosts(1.5, Goods.G_Sausage, 15) --Festivalcosts/Festkosten
-	--The arguments here are multiplicators, that means: 1.5 -> 1.5 times the original cost
-	--Die Argumente hier sind Multiplikatoren, heißt: 1.5 -> 1.5 mal den Originalbetrag
+	--The arguments here are factors, that means: 1.5 -> 1.5 times the original cost
+	--Die Argumente hier sind Faktoren, das heißt: 1.5 -> 1.5 mal den Originalbetrag
 	
 	--[[
 	
-	These are the possible goods you can use as building costs! / Dies sind die möglichen Waren die als Gebäudekosten verwendet werden können!
+		These are the possible goods you can use as building costs! / Dies sind die möglichen Waren die als Gebäudekosten verwendet werden können!
 	
-	Goods.G_Gold,
-	Goods.G_RawFish, Goods.G_Grain, Goods.G_Wood, Goods.G_Iron, Goods.G_Carcass, Goods.G_Stone, Goods.G_Herb, Goods.G_Honeycomb, Goods.G_Wool, Goods.G_Milk,
-	Goods.G_Gems, Goods.G_Dye, Goods.G_Salt, Goods.G_Olibanum, Goods.G_MusicalInstrument
+		Goods.G_Gold,
+		Goods.G_RawFish, Goods.G_Grain, Goods.G_Wood, Goods.G_Iron, Goods.G_Carcass, Goods.G_Stone, Goods.G_Herb, Goods.G_Honeycomb, Goods.G_Wool, Goods.G_Milk,
+		Goods.G_Gems, Goods.G_Dye, Goods.G_Salt, Goods.G_Olibanum, Goods.G_MusicalInstrument
 	
-	Including ALL City Goods from City Buildings, e.g. ... 
-	Inkludierend ALLER Verbrauchsgüter von Stadtgebäuden, zum Beispiel ...
-	
-	Goods.G_Beer, Goods.G_Bread, Goods.G_Broom, Goods.G_Cheese, Goods.G_Clothes, Goods.G_Leather, Goods.G_Medicine,
-	Goods.G_PoorBow, Goods.G_PoorSword, Goods.G_Sausage, Goods.G_SmokedFish, Goods.G_Soap, etc ...
+		Including ALL City Goods from City Buildings, e.g. ... 
+		Inkludierend ALLER Verbrauchsgüter von Stadtgebäuden, zum Beispiel ...
+		
+		Goods.G_Beer, Goods.G_Bread, Goods.G_Broom, Goods.G_Cheese, Goods.G_Clothes, Goods.G_Leather, Goods.G_Medicine,
+		Goods.G_PoorBow, Goods.G_PoorSword, Goods.G_Sausage, Goods.G_SmokedFish, Goods.G_Soap, etc ...
 
-	-> For a full list of goods look in the _G - list!
-	-> Eine vollständige Auflistung aller Güter findet sich in der _G - Liste!
+		-> For a full list of goods look in the _G - list!
+		-> Eine vollständige Auflistung aller Güter findet sich in der _G - Liste!
 
 	]]--
 	
@@ -163,6 +113,10 @@ function SetMyBuildingCosts()
 	BCS.SetCountGoodsOnMarketplace(true)
 end
 
+--[[
+	-> These are just example calls to show how to reset the costs!
+	-> Das hier sind nur Beispielaufrufe um zu zeigen, wie man die Kosten wieder zurücksetzt!
+]]--
 function ResetMyBuildingCosts()
 	-- Reset costs / Kosten zurücksetzen
 	-- Just example calls / Nur Beispielaufrufe
@@ -170,5 +124,51 @@ function ResetMyBuildingCosts()
 	BCS.EditBuildingCosts(UpgradeCategories.BeeHive, nil)
 	BCS.EditTrailCosts(nil)
 	BCS.EditFestivalCosts(nil)
+end
+
+--[[
+	-> You do not have to change anything in this function!
+	-> In dieser Funktion muss nichts verändert werden!
+]]--
+function LoadBuildingCostSystem()
+	-- Initialize BCS --
+	if BCS ~= nil then
+		-- Init System
+		BCS.InitializeBuildingCostSystem()
+		-- Add Savegame when QSB is present
+		if (API and QSB) then
+			-- Register Savegame
+			if API.AddSaveGameAction then -- QSB-S 2.x
+				GUI.SendScriptCommand([[
+					API.AddSaveGameAction(function()
+						Logic.ExecuteInLuaLocalState('BCS.InitializeBuildingCostSystem()')
+					end)
+				]])
+				Framework.WriteToLog("BCS: QSB-S 2.x found! AddSaveGameAction for Savegame registered!")
+			elseif API.AddScriptEventListener and QSB.ScriptEvents ~= nil then -- QSB-S 3.x
+				API.AddScriptEventListener(QSB.ScriptEvents.SaveGameLoaded, BCS.InitializeBuildingCostSystem)
+				API.AddScriptEventListener(QSB.ScriptEvents.BriefingEnded, BCS.OverwriteEndScreenCallback)
+				Framework.WriteToLog("BCS: QSB-S 3.x found! ScriptEventListener for Savegame and BriefingEnded registered!")
+			else
+				Framework.WriteToLog("BCS: QSB-S found, but no Savegame registered. Has to be done manually!")
+			end
+		else
+			Framework.WriteToLog("BCS: QSB-S NOT found! Savegamehandling has to be done manually!")
+		end
+		
+		-- If you don't use the QSB-S, you have to overwrite the Mission_OnSaveGameLoaded() in the global script.
+		-- And call Logic.ExecuteInLuaLocalState('BCS.InitializeBuildingCostSystem()') from there.
+		-- Otherwise all costs are gone after Saveload and the script will not work!
+		
+		-- Sollte die QSB-S nicht verwendet werden, muss Mission_OnSaveGameLoaded() im globalen Skript manuell überschrieben werden.
+		-- Dort muss dann Logic.ExecuteInLuaLocalState('BCS.InitializeBuildingCostSystem()') aufgerufen werden.
+		-- Ansonsten sind nach Saveload alle Kosten weg und das Skript wird nicht funktionieren!
+	else
+		local ErrorMessage = "ERROR: Could not load BuildingCostSystem!"
+		Framework.WriteToLog(ErrorMessage)
+		assert(false, ErrorMessage)
+		return;
+	end
+	-- Done --
 end
 --#EOF
